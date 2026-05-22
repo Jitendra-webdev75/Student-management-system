63a06cf4a4f73ca721eea559366656e68ad971c1
Jitendra <jitendraphone7505@gmail.com>
2026-05-22 11:14:55 +0530

    handle admin

---
+++ pages/homepage/Admin_submit.php
@@ -0,0 +1,37 @@
+
+
+<?php
+session_start();
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// Static admin login
+$admin_email = "monika@gmail.com";
+$admin_password = "12345";
+
+if($email == $admin_email && $password == $admin_password){
+
+    $_SESSION['admin'] = $email;
+
+    echo "
+    <script>
+        alert('Login Successfully');
+
+        window.location.href='/SMS/Project/pages/AdminDash/Admin_Dashboard.html';
+        
+
+    </script>
+    ";
+
+}else{
+
+    echo "
+    <script>
+        alert('Incorrect Email or Password');
+
+        window.location.href='Admin_login.html';
+    </script>
+    ";
+}
+?>
